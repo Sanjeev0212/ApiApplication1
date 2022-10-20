0475b19a3dbb17e72d916b550e70f698afe36a40
Sanjeev0212 <ssinghkushvaha@gmail.com>
2022-10-20 14:15:37 +0530

    all update

---
+++ WebApplication1/WebApplication1/Data/WalksDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Security.Cryptography.X509Certificates;
+using WebApplication1.Models.Domain;
+
+namespace WebApplication1.Data
+{
+    public class WalksDbContext : DbContext
+    {
+        public WalksDbContext(DbContextOptions<WalksDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> walks { get; set; }
+        public DbSet<WalkDifficulty> walkDifficulties { get; set; }
+
+
+
+    }
+}
