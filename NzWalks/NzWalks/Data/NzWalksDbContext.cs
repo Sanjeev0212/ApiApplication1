61f990c28c855e589ab4394f6d584d1945f9a6b8
Sanjeev0212 <ssinghkushvaha@gmail.com>
2022-11-16 12:21:13 +0530

    Create DbContext and database conection in NzWalks API

---
+++ NzWalks/NzWalks/Data/NzWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NzWalks.Models.Domain;
+
+namespace NzWalks.Data
+{
+    public class NzWalksDbContext : DbContext
+    {
+        public NzWalksDbContext(DbContextOptions<NzWalksDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> Walkdifficulty { get; set; }
+    }
+}
