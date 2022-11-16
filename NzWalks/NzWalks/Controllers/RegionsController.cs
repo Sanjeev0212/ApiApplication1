18c65a1e34b62a696df029c9e7a140c1aae071d1
Sanjeev0212 <ssinghkushvaha@gmail.com>
2022-11-16 15:05:27 +0530

    Added New Controller "RegionsController"

---
+++ NzWalks/NzWalks/Controllers/RegionsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NzWalks.Models.Domain;
+
+namespace NzWalks.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            var regions = new List<Region>()
+            {
+                new Region
+                {
+                    Id=Guid.NewGuid(),
+                    Name="Wellington",
+                    Code="WLG",
+                    Area=221202,
+                    Lat=-1.2536,
+                    Long=29.34,
+                    Population=15000,
+                },
+                
+                new Region
+                {
+                    Id=Guid.NewGuid(),
+                    Name="Auckland",
+                    Code="AUCK",
+                    Area=221203,
+                    Lat=-1.5536,
+                    Long=28.34,
+                    Population=16000,
+                }
+            };
+            return Ok(regions);
+        }
+    }
+}
