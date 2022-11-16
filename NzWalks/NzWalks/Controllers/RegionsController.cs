c99e592e328609383873e9e26e2a4b7834c64683
Sanjeev0212 <ssinghkushvaha@gmail.com>
2022-11-16 15:55:19 +0530

    Creating DTOs

--- NzWalks/NzWalks/Controllers/RegionsController.cs
+++ NzWalks/NzWalks/Controllers/RegionsController.cs
@@ -18,7 +18,25 @@ namespace NzWalks.Controllers
         public IActionResult GetAllRegions()
         {
             var regions = regionRepository.GetAll();
-            return Ok(regions);
+
+            //Return Region DTO 
+            var regionsDTO = new List<Models.DTO.Region>();
+            regions.ToList().ForEach(region =>
+            {
+                var regionDTO = new Models.DTO.Region()
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    Area = region.Area,
+                    Lat = region.Lat,
+                    Long = region.Long,
+                    Population = region.Population,
+                };
+                regionsDTO.Add(regionDTO);
+            });
+
+            return Ok(regionsDTO);
         }
     }
 }
